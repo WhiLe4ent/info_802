b93b3d6c86164d303fb44ac748996605ae835c4c
WhiLe4ent <achille.gravouil@gmail.com>
2025-02-13 01:05:43 +0100

    fix CORS origin rules

--- projet_802/src/main/java/com/example/config/CorsConfig.java
+++ projet_802/src/main/java/com/example/config/CorsConfig.java
@@ -14,8 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
-                        .allowedOrigins("https://agreeable-dune-09cad6c10.4.azurestaticapps.net/")
+                        .allowedOrigins("http://localhost:5173", "https://agreeable-dune-09cad6c10.4.azurestaticapps.net/")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
--- projet_802/src/main/java/com/example/config/SoapCorsFilter.java
+++ projet_802/src/main/java/com/example/config/SoapCorsFilter.java
@@ -19,8 +19,9 @@ public class SoapCorsFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
 
-        response.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
-        response.setHeader("Access-Control-Allow-Origin", "https://agreeable-dune-09cad6c10.4.azurestaticapps.net/");
+        String allowedOrigins = "http://localhost:5173, https://agreeable-dune-09cad6c10.4.azurestaticapps.net";
+                
+        response.setHeader("Access-Control-Allow-Origin", allowedOrigins);
         response.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
         response.setHeader("Access-Control-Allow-Headers", "Content-Type, SOAPAction");
         response.setHeader("Access-Control-Allow-Credentials", "true");
